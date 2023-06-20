5122fb1c39e7ac96567f7b31a9cfcc1daef20dc1
amankumarverma <theamankumarverma@gmail.com>
2023-06-20 12:08:29 +0530

    compressed the hero images

--- static/scripts/layout.js
+++ static/scripts/layout.js
@@ -1,15 +1,15 @@
 
 const imagePaths = [
-    '../images/Hero-Background-1.jpeg',
-    '../images/Hero-Background-2.jpeg',
-    '../images/Hero-Background-3.jpeg',
-    '../images/Hero-Background-4.jpeg',
-    '../images/Hero-Background-5.jpeg',
-    '../images/Hero-Background-6.jpeg',
-    '../images/Hero-Background-7.jpeg',
-    '../images/Hero-Background-8.jpeg',
-    '../images/Hero-Background-9.jpeg',
-    '../images/Hero-Background-10.jpeg',
+    '../images/Hero-Img/Hero-Background-1.webp',
+    '../images/Hero-Img/Hero-Background-2.webp',
+    '../images/Hero-Img/Hero-Background-3.webp',
+    '../images/Hero-Img/Hero-Background-4.webp',
+    '../images/Hero-Img/Hero-Background-5.webp',
+    '../images/Hero-Img/Hero-Background-6.webp',
+    '../images/Hero-Img/Hero-Background-7.webp',
+    '../images/Hero-Img/Hero-Background-8.webp',
+    '../images/Hero-Img/Hero-Background-9.webp',
+    '../images/Hero-Img/Hero-Background-10.webp',
 
   ];
   
